2210451752086b0ad0e7141a3d3c7b0445308166
Michael Dietrich <mdietrich@tbz-pariv.de>
2019-02-20 13:08:13 +0100

    add attachFiles function

--- pypdf/pdf.py
+++ pypdf/pdf.py
@@ -36,6 +36,7 @@ See README.md for links to FAQ, documentation, homepage, etc.
 """
 
 import io
+import ntpath
 import random
 import struct
 import time
@@ -385,6 +386,52 @@ class PdfFileWriter(object):
         embeddedFilesDictionary.update({
             NameObject("/EmbeddedFiles"): embeddedFilesNamesDictionary
         })
+        # Update the root
+        self._rootObject.update({
+            NameObject("/Names"): embeddedFilesDictionary
+        })
+
+    def attachFiles(self, files, cut_paths=True):
+        if not isinstance(files, list):
+            files = [files]
+        files_array = ArrayObject()
+
+        for file in files:
+            fname = file
+            if cut_paths:
+                fname = ntpath.basename(fname)
+            fdata = open(file, 'rb').read()
+
+            # The entry for the file
+            file_entry = DecodedStreamObject()
+            file_entry.setData(fdata)
+            file_entry.update({
+                NameObject("/Type"): NameObject("/EmbeddedFile")
+            })
+
+            # The Filespec entry
+            efEntry = DictionaryObject()
+            efEntry.update({NameObject("/F"): file_entry})
+
+            filespec = DictionaryObject()
+            filespec.update({
+                NameObject("/Type"): NameObject("/Filespec"),
+                NameObject("/F"): createStringObject(fname),
+                NameObject("/EF"): efEntry
+            })
+
+            files_array.extend([createStringObject(fname), filespec])
+
+        # The entry for the root
+        embeddedFilesNamesDictionary = DictionaryObject()
+        embeddedFilesNamesDictionary.update({
+            NameObject("/Names"): files_array})
+
+        embeddedFilesDictionary = DictionaryObject()
+        embeddedFilesDictionary.update({
+            NameObject("/EmbeddedFiles"): embeddedFilesNamesDictionary
+        })
+
         # Update the root
         self._rootObject.update({
             NameObject("/Names"): embeddedFilesDictionary
